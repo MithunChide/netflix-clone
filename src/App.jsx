bbc128f00afceceae37dd450d7582cd48711eb5e
Mithun Chide <mithunchide03@gmail.com>
2025-07-09 12:16:35 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Home from './pages/Home/home';
+import Home from './pages/Home/Home';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './pages/Login/Login'
 import Palyer from './pages/Player/Palyer';
